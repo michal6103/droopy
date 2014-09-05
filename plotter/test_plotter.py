5d3cb6e02546ba20b2732cbcf76dc3450cdc6fab
Michal Sedlak <michal6103@gmail.com>
2014-09-05 16:43:08 +0200

    Basic set of tests for plotter

--- plotter/test_plotter.py
+++ plotter/test_plotter.py
@@ -7,14 +7,26 @@ class TestStepper(unittest.TestCase):
 
     def setUp(self):
         self.plotter = plotter.Plotter(debug=True)
+        self.tolerance = self.plotter.steps_per_cm
 
-#    def test_step_top_line(self):
-#        self.plotter.gotoXY(self.plotter.l, 0)
-#        self.assertEqual(self.plotter.getXY(), (self.plotter.l, 0))
+    def test_step_top_line(self):
+        self.plotter.gotoXY(self.plotter.l, 0)
+        self.assertEqual(self.plotter.getXY(), (self.plotter.l, 0))
 
-    def test_step_left_line(self):
-        self.plotter.gotoXY(0, self.plotter.l)
-        self.assertEqual(self.plotter.getXY(), (0, self.plotter.l))
+    def test_left_line(self):
+        x = 0
+        y = self.plotter.l
+        self.plotter.gotoXY(x, y)
+        self.assertTrue(abs(self.plotter.getX() - x) < self.tolerance)
+        self.assertTrue(abs(self.plotter.getY() - y) < self.tolerance)
+
+    def test_every_cm_in_lxl(self):
+        l = int(self.plotter.l)
+        for x in range(l):
+            for y in range(l):
+                self.plotter.gotoXY(x, y)
+                self.assertTrue(abs(self.plotter.getX() - x) < self.tolerance)
+                self.assertTrue(abs(self.plotter.getY() - y) < self.tolerance)
 
 if __name__ == '__main__':
     unittest.main()
