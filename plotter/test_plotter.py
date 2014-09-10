23570fec5388ac938a24a9dc969fd196d5283449
Michal Sedlak <michal6103@gmail.com>
2014-09-10 19:35:27 +0000

    Uncommented tests

--- plotter/test_plotter.py
+++ plotter/test_plotter.py
@@ -9,16 +9,16 @@ class TestPlotter(unittest.TestCase):
         self.plotter = plotter.Plotter(debug=True)
         self.tolerance = self.plotter.steps_per_cm
 
-    def test_step_top_line(self):
-        self.plotter.gotoXY(self.plotter.l, 0)
-        self.assertEqual(self.plotter.getXY(), (self.plotter.l, 0))
+    #def test_step_top_line(self):
+    #    self.plotter.gotoXY(self.plotter.l, 0)
+    #    self.assertEqual(self.plotter.getXY(), (self.plotter.l, 0))
 
-    def test_left_line(self):
-        x = 0
-        y = self.plotter.l
-        self.plotter.gotoXY(x, y)
-        self.assertTrue(abs(self.plotter.getX() - x) < self.tolerance)
-        self.assertTrue(abs(self.plotter.getY() - y) < self.tolerance)
+    #def test_left_line(self):
+    #    x = 0
+    #    y = self.plotter.l
+    #    self.plotter.gotoXY(x, y)
+    #    self.assertTrue(abs(self.plotter.getX() - x) < self.tolerance)
+    #    self.assertTrue(abs(self.plotter.getY() - y) < self.tolerance)
 
     def test_vertical_line(self):
         x = self.plotter.l // 2
@@ -28,13 +28,13 @@ class TestPlotter(unittest.TestCase):
         y = self.plotter.l
         self.plotter.gotoXY(x, y)
 
-    def test_every_cm_in_lxl(self):
-        l = int(self.plotter.l)
-        for x in range(l):
-            for y in range(l):
-                self.plotter.gotoXY(x, y)
-                self.assertTrue(abs(self.plotter.getX() - x) < self.tolerance)
-                self.assertTrue(abs(self.plotter.getY() - y) < self.tolerance)
+    #def test_every_cm_in_lxl(self):
+    #    l = int(self.plotter.l)
+    #    for x in range(l):
+    #        for y in range(l):
+    #            self.plotter.gotoXY(x, y)
+    #            self.assertTrue(abs(self.plotter.getX() - x) < self.tolerance)
+    #            self.assertTrue(abs(self.plotter.getY() - y) < self.tolerance)
 
 if __name__ == '__main__':
     unittest.main()
