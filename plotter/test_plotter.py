8918cfa772e86b29fa421b3fb3fe286b9f3bcee5
Michal Sedlak <michal6103@gmail.com>
2014-09-11 22:13:45 +0200

    All test uncomented

--- plotter/test_plotter.py
+++ plotter/test_plotter.py
@@ -9,29 +9,29 @@ class TestPlotter(unittest.TestCase):
         self.plotter = plotter.Plotter(debug=True)
         self.tolerance = self.plotter.steps_per_cm
 
-    #def test_step_top_line(self):
-    #    self.plotter.gotoXY(self.plotter.l, 0)
-    #    self.assertEqual(self.plotter.getXY(), (self.plotter.l, 0))
+    def test_step_top_line(self):
+        self.plotter.gotoXY(self.plotter.l, 0)
+        self.assertEqual(self.plotter.getXY(), (self.plotter.l, 0))
 
-    #def test_left_line(self):
-    #    x = 0
-    #    y = self.plotter.l
-    #    self.plotter.gotoXY(x, y)
-    #    self.assertTrue(abs(self.plotter.getX() - x) < self.tolerance)
-    #    self.assertTrue(abs(self.plotter.getY() - y) < self.tolerance)
+    def test_left_line(self):
+        x = 0
+        y = self.plotter.l
+        self.plotter.gotoXY(x, y)
+        self.assertTrue(abs(self.plotter.getX() - x) < self.tolerance)
+        self.assertTrue(abs(self.plotter.getY() - y) < self.tolerance)
 
-    #def test_vertical_line(self):
+    def test_vertical_line(self):
         #Initialisation into the middle of the plotter
-    #    x = self.plotter.l // 2
-    #    y = 0
-    #    self.plotter.gotoXY(x, y)
+        x = self.plotter.l // 2
+        y = 0
+        self.plotter.gotoXY(x, y)
 
         #Vertical move
-    #    x = self.plotter.l // 2
-    #    y = self.plotter.l
-    #    self.plotter.gotoXY(x, y)
-    #    self.assertTrue(abs(self.plotter.getX() - x) < self.tolerance)
-    #    self.assertTrue(abs(self.plotter.getY() - y) < self.tolerance)
+        x = self.plotter.l // 2
+        y = self.plotter.l
+        self.plotter.gotoXY(x, y)
+        self.assertTrue(abs(self.plotter.getX() - x) < self.tolerance)
+        self.assertTrue(abs(self.plotter.getY() - y) < self.tolerance)
 
     def test_move_to_same_position(self):
         x = 10
@@ -39,13 +39,13 @@ class TestPlotter(unittest.TestCase):
         self.plotter.gotoXY(x, y)
         self.plotter.gotoXY(x, y)
 
-    #def test_every_cm_in_lxl(self):
-    #    l = int(self.plotter.l)
-    #    for x in range(l):
-    #        for y in range(l):
-    #            self.plotter.gotoXY(x, y)
-    #            self.assertTrue(abs(self.plotter.getX() - x) < self.tolerance)
-    #            self.assertTrue(abs(self.plotter.getY() - y) < self.tolerance)
+    def test_every_cm_in_lxl(self):
+        l = int(self.plotter.l)
+        for x in range(l):
+            for y in range(l):
+                self.plotter.gotoXY(x, y)
+                self.assertTrue(abs(self.plotter.getX() - x) < self.tolerance)
+                self.assertTrue(abs(self.plotter.getY() - y) < self.tolerance)
 
 if __name__ == '__main__':
     unittest.main()
