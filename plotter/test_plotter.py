110bed4e4450348da7bf9e6438ad509ce0525946
Michal Sedlak <michal6103@gmail.com>
2015-03-30 19:44:25 -0400

    Test for draving aevery cm in 1x1 commented out

--- plotter/test_plotter.py
+++ plotter/test_plotter.py
@@ -42,13 +42,13 @@ class TestPlotter(unittest.TestCase):
         self.assertTrue(abs(self.plotter.getX() - x) < self.tolerance)
         self.assertTrue(abs(self.plotter.getY() - y) < self.tolerance)
 
-    def test_every_cm_in_lxl(self):
-        l = int(self.plotter.l)
-        for x in range(l):
-            for y in range(l):
-                self.plotter.gotoXY(x, y)
-                self.assertTrue(abs(self.plotter.getX() - x) < self.tolerance)
-                self.assertTrue(abs(self.plotter.getY() - y) < self.tolerance)
+#    def test_every_cm_in_lxl(self):
+#        l = int(self.plotter.l)
+#        for x in range(l):
+#            for y in range(l):
+#                self.plotter.gotoXY(x, y)
+#                self.assertTrue(abs(self.plotter.getX() - x) < self.tolerance)
+#                self.assertTrue(abs(self.plotter.getY() - y) < self.tolerance)
 
 if __name__ == '__main__':
     unittest.main()
